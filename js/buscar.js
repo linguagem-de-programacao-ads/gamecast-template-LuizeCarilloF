00b5a679422aa2ee8080ad10021617bcb57d01d1
luizeCarilloF <luize.filippini@sptech.school>
2024-04-18 21:17:40 -0300

    aula 3

--- js/buscar.js
+++ js/buscar.js
@@ -1,14 +1,40 @@
-async function buscar(){
+async function buscar() {
     console.log("Passei por aqui")
     const resposta = await fetch("https://660f44ae356b87a55c510d3b.mockapi.io/agendas")
     const respostaDadosAgenda = await resposta.json();
-    console.log("Resposta",respostaDadosAgenda)
+    console.log("Resposta", respostaDadosAgenda)
 
-const cards = window.document.getElementById("cards_games")
+    const cards = window.document.getElementById("cards_games")
 
-cards.innerHTML = respostaDadosAgenda.map((itemAgenda)=> {
-    return "Teste"
-})}
+    cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
+        return `
+    <div class="cardItem">
+    <div class="dataGame"> 
+        <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo}
+    </div>
+    <img class="img-background" src="${itemAgenda.urlImagem}">
+    <div class="descricao">
+        <p> <strong>${itemAgenda.name}</strong> </p>
+        <p> ${itemAgenda.descricao}</p>
+        <p><strong>Gamers: </strong></p>
+
+        <div class="gamers">
+        ${
+            itemAgenda.gamers.map((gamerItem)=> {
+            return `<div class="gamerItem">@${gamerItem}</div>`
+        }).join('')
+    }
+       </div >
+
+      
+    </div >
+        <div class="assistir">
+            <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+        </div>
+</div >
+        `
+}).join('')
+}
 console.log("antes de buscar")
 buscar();
 console.log("depois de buscar")
